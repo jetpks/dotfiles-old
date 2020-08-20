953ccff9849bb214d85c72a43433947f3981176b
Eric Jacobs <eric@jacobs.af>
2020-08-20 16:49:03 -0600

    Rebased on the latest macos and updated Brewfile

--- .install.sh
+++ .install.sh
@@ -3,7 +3,7 @@
 echo "Setting up your Mac..."
 
 # Check for Homebrew and install if we don't have it
-if test ! $(which brew); then
+if ! which brew; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -15,8 +15,8 @@ brew tap homebrew/bundle
 brew bundle
 
 # Make ZSH the default shell environment
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
-source macos
+# source macos
