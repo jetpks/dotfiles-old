a0ecb1bf77a065f69304b5a743c2abfc773f27c7
Eric Jacobs <ejacobs@doximity.com>
2020-09-05 17:24:45 -0600

    Write a real installer script

--- .install.sh
+++ .install.sh
@@ -1,22 +1,67 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Setting up your Mac..."
+## manual steps
+# install command line tools (just try to run git and it'll ask you)
+# Set caps lock to escape
 
-# Check for Homebrew and install if we don't have it
-if ! which brew; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+function main() {
+  make_directories
+  clone_repos
+  create_symlinks
+  brew_install
+  setup_yadr
+  setup_zprezto
+}
 
-# Update Homebrew recipes
-brew update
+function make_directories() {
+  for dir in $HOME/src/github.com/jetpks $HOME/tmp $HOME/bin; do
+    mkdir -p $dir
+  done
+}
 
-# Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
-brew bundle
+# TODO derive the correct paths
+function clone_repos() {
+  for repo in presto yadr dotfiles; do
+    pushd $HOME/src/github.com/jetpks
+    git clone "https://github.com/jetpks/${repo}.git"
+    popd
+  done
+}
 
-# Make ZSH the default shell environment
-# chsh -s $(which zsh)
+function create_symlinks() {
+  local prefix=$HOME/src/github.com/jetpks
+  ln -s $prefix/yadr $HOME/.yadr
+  ln -s $prefix/dotfiles $HOME/.dotfiles
+  ln -s $prefix/presto $HOME/.zprezto
+}
 
-# Set macOS preferences
-# We will run this last because this will reload the shell
-# source macos
+function brew_install() {
+  if ! which brew; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+
+  pushd $HOME/.dotfiles
+  brew update
+  brew tap homebrew/bundle
+  brew bundle
+  popd
+}
+
+function setup_yadr() {
+  pushd $HOME/src/github.com/jetpks/yadr
+  ASK=true rake install
+  popd
+}
+
+function setup_zprezto() {
+  pushd $HOME/.zprezto
+  git submodule update --init --recursive
+  for runcom in zlogin zlogout zpreztorc zprofile zshenv zshrc; do
+    ln -s "$HOME/.zprezto/runcoms/${runcom}" "$HOME/.${runcom}"
+  done
+  popd
+}
+
+main
+
+
